c868307c39178cb4366b2b82fc57042b79ba8559
EC2 Default User <ec2-user@ip-172-31-20-134.ec2.internal>
2017-02-25 18:22:46 +0000

    More parsing changes. Refactoring

--- get_tweets.py
+++ get_tweets.py
@@ -1,44 +1,48 @@
 #import twitter
 from twython import Twython
 import json
+from nltk.corpus import stopwords
 
-# Read in credentials
-with open('credentials.config', 'r') as f:
-    all_credentials = f.readlines()
+class Get_Tweets(object):
 
-# Generate initial twitter object to get Access Token
-twitter = Twython(all_credentials[0][:-1], all_credentials[1][:-1], oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
+    def read_tweets_to_file(self,since_id):
+        # Read in credentials
+        with open('credentials.config', 'r') as f:
+            all_credentials = f.readlines()
 
-# Generate twitter object to make API calls
-twitter = Twython(all_credentials[0][:-1], access_token=ACCESS_TOKEN)
-print "successfully verified credentials"
+        # Generate initial twitter object to get Access Token
+        twitter = Twython(all_credentials[0][:-1], all_credentials[1][:-1], oauth_version=2)
+        ACCESS_TOKEN = twitter.obtain_access_token()
 
-# Check for id of last tweet read
-with open('last_check_info.txt', 'r') as o:
-    since_id = o.readline() 
+        # Generate twitter object to make API calls
+        twitter = Twython(all_credentials[0][:-1], access_token=ACCESS_TOKEN)
+        print "successfully verified credentials"
 
-# Make the API call
-tweets = twitter.get_user_timeline(screen_name='realDonaldTrump', count=200, since_id=since_id)
 
-# If nothing new, exit program
-if len(tweets)==0:
-    print "nothing new"
-    exit()
+        # Make the API call
+        tweets = twitter.get_user_timeline(screen_name='realDonaldTrump', count=200, since_id=since_id)
 
-# Otherwise, save id of most recent tweet
-with open('last_check_info.txt', 'w') as o:
-    o.write(tweets[0]['id_str'])
-    
-# Iterate over tweets
-for tweet in tweets:
-    tweet = tweet.lower().translate(' ', ",;:-+").translate("", "'/\"").replace("..."," ")
-    print "do things"
+        # If nothing new, exit program
+        if len(tweets)==0:
+            print "nothing new"
+            exit()
 
-# Write new recent tweets to file
-with open('recent_tweets', 'w+') as f:
-    for tweet in tweets:
-        tweet = tweet.lower().translate(' ', ",;:-+").translate("", "'/\"").replace("..."," ")
-        to_post = tweet['text'].encode('utf-8').lower()+'\n'
-        f.write(to_post)
+        # Otherwise, save id of most recent tweet
+        with open('last_check_info.txt', 'w') as o:
+            o.write(tweets[0]['id_str'])
+            
+        # Iterate over tweets
+        with open('recent_tweets', 'w+') as f:
+            for tweet in tweets:
+                tweet = tweet['text'].lower().translate(' ', ",;:-+").translate("", "'/\"").replace("..."," ")
+                to_post = tweet['text'].encode('utf-8')+'\n'
+                to_post = [word for word in to_post.split() if word not in stopwords.words('english')]
+                f.write(to_post.join(' '))
+
+if __name__=="__main__":
+    # Check for id of last tweet read
+    with open('last_check_info.txt', 'r') as o:
+        since_id = o.readline() 
+    get_tweets = Get_Tweets()
+    get_tweets.read_tweets_to_file(since_id)
 
